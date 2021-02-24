727f894d8fc3e3fac9243d6612ea9bd3028d5b2d
erkaeunice <77729926+erkaeunice@users.noreply.github.com>
2021-02-24 16:10:07 +0300

    Add files via upload

---
+++ string.py
@@ -0,0 +1,54 @@
+b="orange"
+c="mango"
+print(b+c)
+print(b<=c)
+print(b==c)
+print(b*3)
+print (b is c)
+k1 ="This is anne\'s spam"
+print(k1)
+k="3"
+k2="5"
+print (k+k2)
+k3='Erka'
+letter=b[1]
+print(letter)
+letter=b[-1]
+print(letter) 
+
+k4 ="hello world"
+print(k4 [3:7])
+print(k4[2:4])
+print(k4[:])
+print(k4[:5],k4[1],k4[::2])
+print(k4[-1:3])
+k5 ="Erka"
+k6="Eunice"
+k7=""
+print(len(k5), end=" ")
+print(len(k6), end=" ")
+print(len(k7)) 
+
+r=20
+n=75
+print ('The sum of %d and %d is %d' % (r,n,r+n))
+
+r=20.333
+n=75.76
+print ('The sum of %f and %f is %f' % (r,n,r+n))
+
+r="python"
+n="programming"
+print ('The sum of %s and %s is %s' % (r,n,r+n))
+
+r='hello world'
+print(len(r))
+print(r. upper())
+print(r.lower())
+print(r.replace("h" ,"k"))
+txt="sweatheart" 
+m= txt.center(30)
+print(m)
+txt="good student"
+s=txt.endswith(".")
+print(s)
